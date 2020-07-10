93c7245f4db14bd91733b73b79f8e3a25de51c7a
manukartofanu <manukartofanu@gmail.com>
2020-07-11 01:22:26 +0300

    Add update

--- wpf/src/Manurizer/App.xaml.cs
+++ wpf/src/Manurizer/App.xaml.cs
@@ -1,11 +1,22 @@
-﻿using System.Windows;
+﻿using Manurizer.Views;
+using Squirrel;
+using System.Windows;
 
 namespace ManurizerWpf
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		private async void Application_Startup(object sender, StartupEventArgs e)
+		{
+			using (var mgr = UpdateManager.GitHubUpdateManager("https://github.com/manukartofanu/memorizer"))
+			{
+				await mgr.Result.UpdateApp();
+			}
+			MainWindow window = new MainWindow();
+			window.Show();
+		}
+	}
 }
