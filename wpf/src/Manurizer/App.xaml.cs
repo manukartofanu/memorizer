324ba3f507cb5143f2640892509881e44b30ff66
manukartofanu <manukartofanu@gmail.com>
2020-07-11 01:41:26 +0300

    Remove update from github

--- wpf/src/Manurizer/App.xaml.cs
+++ wpf/src/Manurizer/App.xaml.cs
@@ -9,12 +9,8 @@ namespace ManurizerWpf
 	/// </summary>
 	public partial class App : Application
 	{
-		private async void Application_Startup(object sender, StartupEventArgs e)
+		private void Application_Startup(object sender, StartupEventArgs e)
 		{
-			using (var mgr = UpdateManager.GitHubUpdateManager("https://github.com/manukartofanu/memorizer"))
-			{
-				await mgr.Result.UpdateApp();
-			}
 			MainWindow window = new MainWindow();
 			window.Show();
 		}
