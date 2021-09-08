3063e7966c5effcbfb16e4f02d538a72a65c939b
manukartofanu <manukartofanu@gmail.com>
2021-09-08 19:38:34 +0800

    Show correct text in header of meaning tab

---
+++ wpf/src/Manurizer/Converters/TabToIndexConveter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Manurizer.Converters
+{
+	public class TabToIndexConveter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is TabItem tabItem)
+			{
+				return ItemsControl.ItemsControlFromItemContainer(tabItem).ItemContainerGenerator.IndexFromContainer(tabItem) + 1;
+			}
+			return 0;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return DependencyProperty.UnsetValue;
+		}
+	}
+}
