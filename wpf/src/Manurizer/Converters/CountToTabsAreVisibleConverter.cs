5ec42e7f609e28008b22858f86ff0fbadb9524b3
manukartofanu <manukartofanu@gmail.com>
2021-09-09 20:54:59 +0800

    Show tabs in word dialog smoothly

---
+++ wpf/src/Manurizer/Converters/CountToTabsAreVisibleConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Manurizer.Converters
+{
+	public class CountToTabsAreVisibleConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is int count)
+			{
+				return count > 1;
+			}
+			return false;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return DependencyProperty.UnsetValue;
+		}
+	}
+}
