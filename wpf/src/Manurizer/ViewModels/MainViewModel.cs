ffd5c95776e0b112ed55db82fde08eff19e066f0
manukartofanu <manukartofanu@gmail.com>
2021-10-04 23:58:55 +0800

    Add page controls

--- wpf/src/Manurizer/ViewModels/MainViewModel.cs
+++ wpf/src/Manurizer/ViewModels/MainViewModel.cs
@@ -8,12 +8,41 @@ namespace Manurizer.ViewModels
 	public class MainViewModel : INotifyPropertyChanged
 	{
 		private bool _isNavigatorExpanded = true;
+		private bool _isFirstVisible = true;
+		private bool _isSecondVisible;
+		private bool _isThirdVisible;
+		private bool _isFourthVisible;
 		
 		public ICommand NavigatorExpandCommand { get; private set; }
+		public ICommand SelectControlCommand { get; private set; }
 
 		public MainViewModel()
 		{
 			NavigatorExpandCommand = new RelayCommand((t) => { IsNavigatorExpanded = !IsNavigatorExpanded;});
+			SelectControlCommand = new RelayCommand((t) => { SelectControl((int)t); });
+		}
+
+		private void SelectControl(int index)
+		{
+			IsFirstVisible = false;
+			IsSecondVisible = false;
+			IsThirdVisible = false;
+			IsFourthVisible = false;
+			switch (index)
+			{
+				case 0:
+					IsFirstVisible = true;
+					break;
+				case 1:
+					IsSecondVisible = true;
+					break;
+				case 2:
+					IsThirdVisible = true;
+					break;
+				case 3:
+					IsFourthVisible = true;
+					break;
+			}
 		}
 
 		public bool IsNavigatorExpanded
@@ -23,6 +52,46 @@ namespace Manurizer.ViewModels
 			{
 				_isNavigatorExpanded = value;
 				RaisePropertyChanged(nameof(IsNavigatorExpanded));
+			}
+		}
+
+		public bool IsFirstVisible
+		{
+			get { return _isFirstVisible; }
+			set
+			{
+				_isFirstVisible = value;
+				RaisePropertyChanged(nameof(IsFirstVisible));
+			}
+		}
+
+		public bool IsSecondVisible
+		{
+			get { return _isSecondVisible; }
+			set
+			{
+				_isSecondVisible = value;
+				RaisePropertyChanged(nameof(IsSecondVisible));
+			}
+		}
+
+		public bool IsThirdVisible
+		{
+			get { return _isThirdVisible; }
+			set
+			{
+				_isThirdVisible = value;
+				RaisePropertyChanged(nameof(IsThirdVisible));
+			}
+		}
+
+		public bool IsFourthVisible
+		{
+			get { return _isFourthVisible; }
+			set
+			{
+				_isFourthVisible = value;
+				RaisePropertyChanged(nameof(IsFourthVisible));
 			}
 		}
 
